8fcd2d375ed58d25bd8b1722a564994b6be108f6
Aldomar Assolin <70400399+AldomarAssolin@users.noreply.github.com>
2026-09-02 23:41:34 -0300

    Atualizar o gerar_senha.php

--- gerar_senha.php
+++ gerar_senha.php
@@ -1,5 +1,5 @@
 <?php
-$senha = "319Manex!"; // você escolhe a senha
+$senha = "34567"; // você escolhe a senha
 $hash = password_hash($senha, PASSWORD_DEFAULT);
 echo "Senha: $senha<br>";
 echo "Hash gerada: $hash";
